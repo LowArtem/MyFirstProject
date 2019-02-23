2da0aa8542f4ff6dff09fc2361e2453388b3dfa0
LowArtem <artemziharew@gmail.com>
2019-02-23 20:22:46 +0400

    Добавлены забытые функции
    Выход из меню выбора скорости в меню игры осуществлено через функц. клавиши, и красную кнопку

---
+++ MyFirstProject/Assets/Scripts/AndroidKeySpeedControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AndroidKeySpeedControl : MonoBehaviour {
+
+
+	void Update () 
+	{
+		if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+				SceneManager.LoadScene("Menu");
+			}
+		}
+	}
+}
